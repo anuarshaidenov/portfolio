7b43e19d5d07871e54221568f76d9955f42266cc
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-11-09 14:06:24 +0600

    Add transition to mobile menu

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const btnHamburger = document.getElementById('hamburger');
-const btnClose = document.getElementById('close');
-const menu = document.getElementById('menu');
-const links = document.querySelectorAll('.mobile-nav__el a');
+const btnHamburger = document.getElementById("hamburger");
+const btnClose = document.getElementById("close");
+const menu = document.getElementById("menu");
+const links = document.querySelectorAll(".mobile-nav__el a");
 
 const openMenu = function () {
-  btnHamburger.classList.add('hidden');
-  btnClose.classList.remove('hidden');
-  menu.classList.remove('hidden');
+  btnHamburger.classList.add("hidden");
+  btnClose.classList.remove("hidden");
+  menu.classList.remove("translated");
 };
 
 const closeMenu = function () {
-  btnClose.classList.add('hidden');
-  btnHamburger.classList.remove('hidden');
-  menu.classList.add('hidden');
+  btnClose.classList.add("hidden");
+  btnHamburger.classList.remove("hidden");
+  menu.classList.add("translated");
 };
 
-btnHamburger.addEventListener('click', openMenu);
-btnClose.addEventListener('click', closeMenu);
+btnHamburger.addEventListener("click", openMenu);
+btnClose.addEventListener("click", closeMenu);
 links.forEach((link) => {
-  link.addEventListener('click', closeMenu);
+  link.addEventListener("click", closeMenu);
 });
