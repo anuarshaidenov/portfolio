2142d2b0d7f2af8174172da1ad6fa571a8260acf
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-11-10 20:36:37 +0600

    Fix form validation script and add error animation

--- js/formValidation.js
+++ js/formValidation.js
@@ -2,19 +2,19 @@ const email = document.querySelector('#email');
 const form = document.querySelector('#form');
 const errorMsg = document.querySelector('#error-msg');
 
-const emailValidationRegex = /^[a-z]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-
-function emailCheck(emailValue) {
-  return !emailValidationRegex.test(emailValue) ? false : true ;
+function isEmailLowerCase(emailValue) {
+  return emailValue === emailValue.toLowerCase() ? true : false;
 }
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    if(emailCheck(email.value) === true){
-        errorMsg.style.opacity = 1;
-        errorMsg.textContent = 'enter a valid email please !';
-    }else {
-        errorMsg.style.opacity = 0;
-        form.submit();
-    }
-})
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (isEmailLowerCase(email.value)) {
+    errorMsg.classList.add('invisible');
+    errorMsg.classList.remove('shake');
+    form.submit();
+  } else {
+    errorMsg.textContent = 'Email must be typed in lowercase.';
+    errorMsg.classList.remove('invisible');
+    errorMsg.classList.add('shake');
+  }
+});
